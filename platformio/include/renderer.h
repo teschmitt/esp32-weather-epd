25c76ffcd4f3d1fcbbfb1304581be7bd7de4a4f1
Thomas Schmitt <t.e.schmitt@posteo.de>
2024-03-07 10:49:58 +0100

    experiment with alternative display library

--- platformio/include/renderer.h
+++ platformio/include/renderer.h
@@ -24,12 +24,12 @@
 #include "api_response.h"
 #include "config.h"
 
-#ifdef DISP_BW_400x300
+#ifdef DISP_BW_420
   #define DISP_WIDTH  400
   #define DISP_HEIGHT 300
   #include <GxEPD2_BW.h>
-  extern GxEPD2_BW<GxEPD2_420_M01,
-                   GxEPD2_420_M01::HEIGHT> display;
+  extern GxEPD2_BW<GxEPD2_420,
+                   GxEPD2_420::HEIGHT> display;
 #endif
 #ifdef DISP_BW_V2
   #define DISP_WIDTH  800
